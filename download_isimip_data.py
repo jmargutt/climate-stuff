997bf60ef54072e6bdf99823da4d9293608e830e
jmargutti <jacopo.margutti@gmail.com>
2022-09-21 17:46:36 +0200

    u

--- download_isimip_data.py
+++ download_isimip_data.py
@@ -1,6 +1,7 @@
 import os
 import urllib.request
 from pathlib import Path
+import tqdm
 
 home = str(Path.home())
 root_dir = f"{home}/climate-change/ISIMIP data"
@@ -8,8 +9,12 @@ text_file = open(f"{root_dir}/filelist.txt", "r")
 file_urls = text_file.read().split('\n')
 print(file_urls)
 
-for file_url in file_urls:
+for file_url in tqdm(file_urls):
     file_path = file_url.replace('https://files.isimip.org', root_dir)
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    urllib.request.urlretrieve(file_url, file_path)
+    if not os.path.exists(file_path):
+        try:
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            urllib.request.urlretrieve(file_url, file_path)
+        except:
+            print('error in creating file:', file_path)
 
