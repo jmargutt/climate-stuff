a1e57ad4f84a8208393892e3d082ba3f85b74116
jmargutti <jacopo.margutti@gmail.com>
2022-10-21 16:22:53 +0200

    u

---
+++ slice_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# NOTE : Quote it else use array to avoid problems #
+FILES="/home/geo/climate-change/'ISIMIP data2'/**/*.nc"
+for f in $FILES
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  python slice_isimip_data.py --file "$f"
+done
--- slice_isimip_data.py
+++ slice_isimip_data.py
@@ -5,30 +5,41 @@ import gc
 from pathlib import Path
 from tqdm import tqdm
 import os
+import click
 
-def slice_file(file, bbox, dest):
-    with xr.open_dataset(file) as ds:
-        ds.sel(lat=slice(bbox[3], bbox[1]), lon=slice(bbox[0], bbox[2])).to_netcdf(dest)
-        ds.close()
-        del ds
-        gc.collect()
-
-
-if __name__ == '__main__':
-
-    home = str(Path.home())
-    data_dir = "ISIMIP data2"
-    root_dir = f"{home}/climate-change/{data_dir}"
-    nc_files = glob.glob(f'{root_dir}/**/*.nc', recursive=True)
-    nc_files = list(set(nc_files))
+@click.command()
+@click.option('--file', help='File name.')
+def slice_file(file):
 
     ## load shapefile of area
     boundary = 'Burkina_Faso_livilihoodzones.geojson'
     bf_gpd = gpd.read_file(boundary)
     ## convert into bounding box
-    bbox_bfs = list(bf_gpd.total_bounds)
+    bbox = list(bf_gpd.total_bounds)
 
-    for file in tqdm(nc_files):
-        dest = file.replace(data_dir, 'ISIMIP data BFA')
-        os.makedirs(os.path.dirname(dest), exist_ok=True)
-        slice_file(file, bbox_bfs, dest)
+    data_dir = "ISIMIP data2"
+    dest = file.replace(data_dir, 'ISIMIP data BFA')
+    os.makedirs(os.path.dirname(dest), exist_ok=True)
+
+    with xr.open_dataset(file) as ds:
+        ds.sel(lat=slice(bbox[3], bbox[1]), lon=slice(bbox[0], bbox[2])).to_netcdf(dest)
+
+
+if __name__ == '__main__':
+    slice_file()
+    # home = str(Path.home())
+    # data_dir = "ISIMIP data2"
+    # root_dir = f"{home}/climate-change/{data_dir}"
+    # nc_files = glob.glob(f'{root_dir}/**/*.nc', recursive=True)
+    # nc_files = list(set(nc_files))
+    #
+    # ## load shapefile of area
+    # boundary = 'Burkina_Faso_livilihoodzones.geojson'
+    # bf_gpd = gpd.read_file(boundary)
+    # ## convert into bounding box
+    # bbox_bfs = list(bf_gpd.total_bounds)
+    #
+    # for file in tqdm(nc_files):
+    #     dest = file.replace(data_dir, 'ISIMIP data BFA')
+    #     os.makedirs(os.path.dirname(dest), exist_ok=True)
+    #     slice_file(file, bbox_bfs, dest)
