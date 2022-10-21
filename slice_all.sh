c12df974d6f33ec0cedbba01cb1f3ef887ffc9eb
jmargutti <jacopo.margutti@gmail.com>
2022-10-21 16:31:21 +0200

    u

--- slice_all.sh
+++ slice_all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # NOTE : Quote it else use array to avoid problems #
-for f in in /home/geo/climate-change/ISIMIP-data2/**/*.nc ;
+DIR="/home/geo/climate-change/ISIMIP-data2/"
+for f in "$DIR"/**/*.nc
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  python3 slice_isimip_data.py --file "$f"
+#  python3 slice_isimip_data.py --file "$f"
 done
