fcdc8bebc7748a2e5984085412e82d2006210278
jmargutti <jacopo.margutti@gmail.com>
2022-10-21 16:29:33 +0200

    u

--- slice_all.sh
+++ slice_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # NOTE : Quote it else use array to avoid problems #
-for f in in /home/geo/climate-change/ISIMIP-data2/**/*.nc
+for f in in /home/geo/climate-change/ISIMIP-data2/**/*.nc ;
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
