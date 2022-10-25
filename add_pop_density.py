cb873e545f77898004e50ebb2a81d4184bc03a13
jmargutti <jacopo.margutti@gmail.com>
2022-10-25 17:36:12 +0200

    u

--- add_pop_density.py
+++ add_pop_density.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import pycountry
 import os
 import click
+import subprocess
 from gdal_polygonize import gdal_polygonize
 
 engine = create_engine(
@@ -39,21 +40,37 @@ def add_pop_density(temp):
             print('download failed')
 
         try:
-            print(f'transforming from raster to vector {country.name}')
-            filepath_vector = filepath.replace('.tif', '.geojson')
-            gdal_polygonize(src_filename=filepath, dst_filename=filepath_vector, driver_name="GeoJSON")
-        except:
-            print('gdal_polygonize failed')
-
-        try:
             if os.path.exists('query.sql'):
                 os.remove('query.sql')
             print(f'uploading {country.name}')
-            gdf = gpd.read_file(filepath_vector)
-            gdf = gdf.rename(columns={'DN': 'population'})
-            gdf.to_postgis(f"{country.alpha_3.lower()}_ppp_2020_UNadj_constrained", engine, schema="population_density")
+            os.environ['PGPASSWORD'] = ''
+            ###################################################################
+            # WARNING: FOR UPDATING EXISTING TABLES, USE OPTION raster2pgsql -d
+            ###################################################################
+            cmds = 'raster2pgsql -t auto -I -s 4326 "' + filepath + f'" population_density.{filename.replace(".tif", "")} > query.sql'
+            subprocess.call(cmds, shell=True, stdout=subprocess.DEVNULL)
+            cmds = "psql -f query.sql -h 510-postgresql-server.postgres.database.azure.com -U jmargutti@510-postgresql-server -d global510"
+            subprocess.call(cmds, shell=True, stdout=subprocess.DEVNULL)
+            os.remove('query.sql')
         except:
             print('upload failed')
+
+        # try:
+        #     print(f'transforming from raster to vector {country.name}')
+        #     filepath_vector = filepath.replace('.tif', '.geojson')
+        #     gdal_polygonize(src_filename=filepath, dst_filename=filepath_vector, driver_name="GeoJSON")
+        # except:
+        #     print('gdal_polygonize failed')
+        #
+        # try:
+        #     if os.path.exists('query.sql'):
+        #         os.remove('query.sql')
+        #     print(f'uploading {country.name}')
+        #     gdf = gpd.read_file(filepath_vector)
+        #     gdf = gdf.rename(columns={'DN': 'population'})
+        #     gdf.to_postgis(f"{country.alpha_3.lower()}_ppp_2020_UNadj_constrained", engine, schema="population_density")
+        # except:
+        #     print('upload failed')
 
         for file_ in [filepath, filepath_vector]:
             if os.path.exists(file_):
