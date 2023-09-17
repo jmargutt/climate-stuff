0c154021918d6c66dc0348e284e0b0446a72ac32
jmargutti <jacopo.margutti@gmail.com>
2023-09-17 12:24:59 +0400

    test

--- slice_isimip_data.py
+++ slice_isimip_data.py
@@ -5,27 +5,37 @@ import gc
 from pathlib import Path
 from tqdm import tqdm
 import os
+import click
 
 
 def slice_file(file, bbox, dest):
     with xr.open_dataset(file) as ds:
         ds.sel(lat=slice(bbox[3], bbox[1]), lon=slice(bbox[0], bbox[2])).to_netcdf(dest)
-
-
-if __name__ == '__main__':
-    home = str(Path.home())
-    data_dir = "ISIMIP-data"
-    root_dir = f"{home}/climate-change/{data_dir}"
-    nc_files = glob.glob(f'{root_dir}/**/*.nc', recursive=True)
+        
+@click.command()
+@click.option('--adminbound', default='extents.geojson', help='country boundaries')
+@click.option('--input', default='ISIMIP-data', help='country boundaries')
+@click.option('--output', default='countries', help='country boundaries')
+def main(adminbound, input, output)
+    nc_files = glob.glob(f'{input}/**/*.nc', recursive=True)
     nc_files = list(set(nc_files))
 
     # load shapefile of area
-    boundary = 'Burkina_Faso_livilihoodzones.geojson'
-    bf_gpd = gpd.read_file(boundary)
-    # convert into bounding box
-    bbox_bfs = list(bf_gpd.total_bounds)
+    gdf = gpd.read_file(adminbound)
+    
+    for ix, row in tqdm(gdf.iterrows(), total=len(gdf)):
+        # convert into bounding box
+        name = row['iso3']
+        if name == 'None' or len(name) != 3:
+            continue
+        bbox = list(row.total_bounds)
 
-    for file in tqdm(nc_files):
-        dest = file.replace(data_dir, 'ISIMIP-data-BFA')
-        os.makedirs(os.path.dirname(dest), exist_ok=True)
-        slice_file(file, bbox_bfs, dest)
+        for file in tqdm(nc_files):
+            country_file = file.replace(input, os.path.join(output, name))
+            print(country_file)
+
+            # os.makedirs(os.path.dirname(dest_contry), exist_ok=True)
+            # slice_file(file, bbox, dest_contry)
+        
+if __name__ == "__main__":
+    main()
